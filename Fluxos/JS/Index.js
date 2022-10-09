132cd2dd5714b461c4d9d3d630b5cb9641c46653
Gustavo <gustavoms7@al.insper.edu.br>
2022-10-09 04:21:58 -0300

    Distripida

---
+++ Fluxos/JS/Index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+    localStorage.setItem('tsegunda', 0)
+    localStorage.setItem('tterca', 0)
+    localStorage.setItem('tquarta', 0)
+    localStorage.setItem('tquinta', 0)
+    localStorage.setItem('tsexta', 0)
+    localStorage.setItem('tsabado', 0)
+    localStorage.setItem('tdomingo', 0)
+    localStorage.setItem('diaatual', 0)
+    localStorage.setItem('novotreino', '')
+    
+
+})
